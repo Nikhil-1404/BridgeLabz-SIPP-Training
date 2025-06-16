39021e75c0ff20e67ee984cf142d65935f83071d
Nikhil-1404 <ny480240@gmail.com>
2025-06-16 09:22:18 +0530

    [Nikhil Kumar].Add Problems For the day2 of Javafundamentals

---
+++ JavaFundamentals/HeightConverter.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class HeightConverter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your height in centimeters: ");
+        double cm = scanner.nextDouble();
+
+        double totalInches = cm / 2.54;
+        int feet = (int) (totalInches / 12);
+        double inches = totalInches % 12;
+
+        System.out.printf("Your Height in cm is %.2f while in feet is %d and inches is %.2f\n", cm, feet, inches);
+    }
+}
---
+++ JavaFundamentals/SampleProgram2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SampleProgram2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        String fromCity = sc.nextLine();
+        String viaCity = sc.nextLine();
+        String toCity = sc.nextLine();
+        double fromToVia = sc.nextDouble();
+        double viaToFinalCity = sc.nextDouble();
+        double timeFromToVia = sc.nextDouble();
+        double timeViaToFinalCity = sc.nextDouble();
+        double totalDistance = fromToVia + viaToFinalCity;
+        double totalTime = timeFromToVia + timeViaToFinalCity;
+        System.out.println("The Total Distance travelled by " + name + " from " + fromCity + " to " + toCity + " via " + viaCity + " is " + totalDistance + " km and the Total Time taken is " + totalTime + " minutes");
+    }
+}
